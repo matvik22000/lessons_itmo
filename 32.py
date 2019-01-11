68d0397ba5520a45fc3668a0ff9a21dedadb1401
Matvey <matvik.gurov@gmail.com>
2019-01-11 18:11:54 +0300

    123

--- 32.py
+++ 32.py
@@ -1,25 +1,31 @@
-arr = [
-    [0, 1, 1],
-    [1, 0, 0],
-    [1, 0, 0]
-]
-queue = []
-levels = []
-for i in range(0, len(arr)):
+n = int(input())
+arr = []
+for i in range(n):
+    arr1 = []
+    for j in range(n):
+        arr1.append(0)
+    arr.append(arr1)
+
+for i in range(1, n):
+    a = input().split()
+    for j in a:
+        arr[int(j) - 1][i] = 1
+
+start = 0
+levels = [[start]]
+while True:
     levels.append([])
-print(levels)
-used = [False] * 6
-queue.append([0, 0])
-while len(queue) > 0:
-    el = queue.pop(0)
-    levels[el[1]].append(el[0])
-    if not used[el[0]]:
-        print(el[0] + 1)
-        used[el[0]] = True
-        for i in range(len(arr[el[0]])):
-            if arr[el[0]][i] == 1 and not used[i]:
-                queue.append([i, el[1] + 1])
+    for j in range(len(levels[-2])):
+        for k in range(len(arr[levels[-2][j]])):
+            if arr[levels[-2][j]][k] == 1:
+                levels[-1].append(k)
 
+    if len(levels[-1]) == 0:
+        break
 
-print(levels)
+print(len(levels) - 2)
+print(len(levels[-2]))
+levels[-2].sort()
+for i in levels[-2]:
+    print(i + 1, end=' ')
 
